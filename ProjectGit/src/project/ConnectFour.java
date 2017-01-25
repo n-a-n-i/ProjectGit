de94e256555bd4335c1c744508cfc53efd8251d0
n-a-n-i <n.j.huitink@student.utwente.nl>
2017-01-25 17:37:00 +0100

    Server en Client kunnen een game starten

--- ProjectGit/src/project/ConnectFour.java
+++ ProjectGit/src/project/ConnectFour.java
@@ -8,14 +8,17 @@ public class ConnectFour {
 	 * 	 ensures dim == dimension;
 	 *   ensures player1 != null && player2 != null;
 	 */
-	public static void main(String[] args) {//String p1, String p2, int dimension
+	public static void main(String p1, String p2, int dimension) { //String p1, String p2, int dimension
 		Player player1;
 		Player player2;
 
-		dim = Integer.parseInt(args[2]);//dimension
+		dim = dimension; //Integer.parseInt(args[2]);//dimension
 
-		player1 = new ComputerPlayer(args[0], Mark.OOO);//p1
-		player2 = new ComputerPlayer(args[1], Mark.XXX);//p2
+		player1 = new HumanPlayer(p1, Mark.OOO); //p1
+		player2 = new HumanPlayer(p2, Mark.XXX); //p2
+		
+//		player1 = new ComputerPlayer(p1, Mark.OOO); //p1
+//		player2 = new ComputerPlayer(p2, Mark.XXX); //p2
 
 		Game game = new Game(player1, player2);
 		game.start();
